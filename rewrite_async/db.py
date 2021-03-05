edbb0ab76bd5e7d3843dafd56ab86c44601d8f30
nnnocturn <vikchehovich@gmail.com>
2021-03-05 14:28:57 +0200

    rm all txt?

--- rewrite_async/db.py
+++ rewrite_async/db.py
@@ -12,9 +12,19 @@ def start_db():
     conn.commit()
 
 
-def insert_db(chat_id,locale,info,list_user):
+def insert_db(chat_id,locale="none",info="none",list_user="none"):
     conn = sqlite3.connect('bots.db')
     cur = conn.cursor()
-    user = (chat_id, locale, info, list_user)
-    cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?);", user)
+    if locale == "none" and info == "none" and list_user == "none":
+        user = (chat_id, locale, info, list_user)
+        cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?);", user)
+    elif locale != "none":
+        sql = """UPDATE ListMembers SET locale = %s WHERE chat_id = %s"""
+        cur.execute(sql, (locale, chat_id))
+    elif info != "none":
+        sql = """UPDATE ListMembers SET info = %s WHERE chat_id = %s"""
+        cur.execute(sql, (locale, chat_id))
+    elif list_user != "none":
+        sql = """UPDATE ListMembers SET list_user = %s WHERE chat_id = %s"""
+        cur.execute(sql, (locale, chat_id))
     conn.commit()
